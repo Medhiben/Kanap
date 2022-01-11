f5450759e554b9706fc3043ab6d07948d15773a4
Medhiben <91384959+Medhiben@users.noreply.github.com>
2022-01-11 16:53:56 +0100

    modification PPT

---
+++ P5_RAMDANI_code/front/js/confirmation.js
@@ -0,0 +1,11 @@
+const orderId = localStorage.getItem("orderId");
+console.log(orderId)
+ if(orderId) {
+    const idNode = document.getElementById("orderId");
+    idNode.innerText = localStorage.getItem("orderId");
+    console.log(localStorage.getItem("orderId"));
+    localStorage.clear();
+
+}else{
+  document.querySelector(".confirmation p").innerHTML = "Commande incorrecte"
+}
