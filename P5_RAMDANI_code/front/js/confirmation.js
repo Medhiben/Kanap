8fb0fc508a2a47b0621842eddadbc4bf89220d54
Medhiben <91384959+Medhiben@users.noreply.github.com>
2022-01-12 17:35:36 +0100

    indentation et correction des erreurs HTML CSS par W3C

--- P5_RAMDANI_code/front/js/confirmation.js
+++ P5_RAMDANI_code/front/js/confirmation.js
@@ -1,11 +1,11 @@
 const orderId = localStorage.getItem("orderId");
 console.log(orderId)
- if(orderId) {
-    const idNode = document.getElementById("orderId");
-    idNode.innerText = localStorage.getItem("orderId");
-    console.log(localStorage.getItem("orderId"));
-    localStorage.clear();
+if (orderId) {
+  const idNode = document.getElementById("orderId");
+  idNode.innerText = localStorage.getItem("orderId");
+  console.log(localStorage.getItem("orderId"));
+  localStorage.clear();
 
-}else{
+} else {
   document.querySelector(".confirmation p").innerHTML = "Commande incorrecte"
 }
